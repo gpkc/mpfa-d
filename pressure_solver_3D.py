702334ffd0d1817d9ac63b129c6200c543baa2cd
Sidicley <sidicleyribeiro@gmail.com>
2018-02-25 09:11:45 -0300

    Adding inv dist interp and add'nal reestr'ing for intern nodes treatment

--- pressure_solver_3D.py
+++ pressure_solver_3D.py
@@ -35,8 +35,13 @@ class MpfaD3D:
         self.neumann_faces = mesh_data.neumann_faces
 
         self.all_faces = self.mb.get_entities_by_dimension(0, 2)
+        boundary_faces = (self.dirichlet_faces | self.neumann_faces)
         # print('ALL FACES', all_faces, len(all_faces))
-        self.intern_faces = set(self.all_faces) - (self.dirichlet_faces | self.neumann_faces)
+        self.intern_faces = set(self.all_faces) - boundary_faces
+
+        self.volumes = self.mesh_data.all_volumes
+        self.A = np.zeros([len(self.volumes), len(self.volumes)])
+        self.b = np.zeros([1, len(self.volumes)])
 
     def area_vector(self, JK, JI, test_vector=np.zeros(3)):
         N_IJK = np.cross(JK, JI) / 2.0
@@ -78,17 +83,36 @@ class MpfaD3D:
             phys_aniso_term
         return cross_term
 
-    def run(self):
+    def get_intern_nodes_weights(self, method):
+        nodes_weights = {}
+        for a_node in self.intern_nodes:
+            nodes_weights[a_node] = method(a_node)
+        return nodes_weights
+
+    def _node_treatment(self, node, nodes_weights, id_1st, id_2nd, v_ids,
+                        transm, cross_1st, cross_2nd=0.0, is_J=1):
+        if node in self.dirichlet_nodes:
+            pressure = self.mesh_data.mb.tag_get_data(self.dirichlet_tag, node)
+            value = (is_J) * transm * (cross_1st + cross_2nd) * pressure
+            self.b[0][id_1st] += value
+            self.b[0][id_2nd] += - value
+            # print('VALOR', value)
+        if node in self.intern_nodes:
+            for volume, weight in nodes_weights[node].items():
+                v_id = v_ids[volume]
+                value = (is_J) * transm * (cross_1st + cross_2nd) * weight
+                self.A[id_1st][v_id] += - value
+                self.A[id_2nd][v_id] += value
+
+    def run(self, interpolation_method):
+
+        nodes_weights = self.get_intern_nodes_weights(interpolation_method)
 
         v_ids = self.set_global_id()
 
-        for a_node in self.intern_nodes | self.neumann_nodes:
+        for a_node in self.neumann_nodes:  # | self.intern_nodes:
             a_node_coords = self.mb.get_coords([a_node])[0]
             self.mb.tag_set_data(self.dirichlet_tag, a_node, 1.0 - a_node_coords)
-
-        volumes = self.mesh_data.all_volumes
-        A = np.zeros([len(volumes), len(volumes)])
-        b = np.zeros([1, len(volumes)])
 
         for face in self.all_faces:
 
@@ -96,10 +120,11 @@ class MpfaD3D:
                 face_flow = self.mb.tag_get_data(self.neumann_tag, face)
                 volume = self.mtu.get_bridge_adjacencies(face, 2, 3)
                 volume = np.asarray(volume, dtype='uint64')
-                b[0][v_ids[volume[0]]] += -face_flow
+                self.b[0][v_ids[volume[0]]] += -face_flow
 
             if face in self.dirichlet_faces:
-                volume = np.asarray(self.mtu.get_bridge_adjacencies(face, 2, 3), dtype='uint64')
+                volume = self.mtu.get_bridge_adjacencies(face, 2, 3)
+                volume = np.asarray(volume, dtype='uint64')
                 volume_centroid = self.mtu.get_average_position(volume)
 
                 I, J, K = self.mtu.get_bridge_adjacencies(face, 0, 0)
@@ -137,20 +162,18 @@ class MpfaD3D:
                         K_R_n / h_R * g_J)
 
                 volume_id = v_ids[volume[0]]
-                A[volume_id][volume_id] += -2 * K_n_eff
-                b[0][volume_id] += RHS
+                self.A[volume_id][volume_id] += -2 * K_n_eff
+                self.b[0][volume_id] += RHS
 
             if face in self.intern_faces:
                 face_centroid = self.mtu.get_average_position([face])
-                left_volume, right_volume = self.mtu.get_bridge_adjacencies(face, 2, 3)
-                left_volume_centroid = self.mtu.get_average_position([left_volume])
-                right_volume_centroid = self.mtu.get_average_position([right_volume])
-                # test_LR = left_volume_centroid - right_volume_centroid
+                left_volume, right_volume = \
+                    self.mtu.get_bridge_adjacencies(face, 2, 3)
+                left_vol_cent = self.mtu.get_average_position([left_volume])
+                right_vol_cent = self.mtu.get_average_position([right_volume])
+                # test_LR = left_vol_cent - right_vol_cent
 
                 I, J, K = self.mtu.get_bridge_adjacencies(face, 0, 0)
-                p_I = self.mb.tag_get_data(self.dirichlet_tag, I)
-                p_J = self.mb.tag_get_data(self.dirichlet_tag, J)
-                p_K = self.mb.tag_get_data(self.dirichlet_tag, K)
                 JI = self.mb.get_coords([I]) - self.mb.get_coords([J])
                 JK = self.mb.get_coords([K]) - self.mb.get_coords([J])
 
@@ -160,14 +183,14 @@ class MpfaD3D:
                 # check_left_or_right = np.dot(test_LR, N_IJK)
                 # if check_left_or_right < 0:
                 #     right_volume, left_volume = left_volume, right_volume
-                #     right_volume_centroid, left_volume_centroid = left_volume_centroid, right_volume_centroid
+                #     right_vol_cent, left_vol_cent = left_vol_cent, right_vol_cent
 
-                dist_LR = right_volume_centroid - left_volume_centroid
+                dist_LR = right_vol_cent - left_vol_cent
                 tan_JI = np.cross(JI, N_IJK)
                 tan_JK = np.cross(N_IJK, JK)
 
                 K_R = self.mb.tag_get_data(self.perm_tag, right_volume).reshape([3, 3])
-                h_R = face_centroid - right_volume_centroid
+                h_R = face_centroid - right_vol_cent
                 h_R = np.absolute(np.dot(N_IJK, h_R) / np.sqrt(np.dot(N_IJK,
                                                                       N_IJK)))
 
@@ -176,7 +199,7 @@ class MpfaD3D:
                 K_R_JK = self._flux_term(N_IJK, K_R, tan_JK, face_area)
 
                 K_L = self.mb.tag_get_data(self.perm_tag, left_volume).reshape([3, 3])
-                h_L = face_centroid - left_volume_centroid
+                h_L = face_centroid - left_vol_cent
                 h_L = np.absolute(np.dot(N_IJK, h_L) / np.sqrt(np.dot(N_IJK,
                                                                       N_IJK)))
 
@@ -195,28 +218,36 @@ class MpfaD3D:
                                                h_R, h_L)
 
                 K_n_eff = K_R_n * K_L_n / (K_R_n * h_L + K_L_n * h_R)
-                RHS = K_n_eff * (D_JI * (p_I - p_J) + D_JK * (p_K - p_J))
 
                 gid_right = v_ids[right_volume]
                 gid_left = v_ids[left_volume]
 
-                A[gid_right][gid_right] += -2 * K_n_eff
-                A[gid_right][gid_left] += 2 * K_n_eff
+                self.A[gid_right][gid_right] += -2 * K_n_eff
+                self.A[gid_right][gid_left] += 2 * K_n_eff
 
-                b[0][gid_right] += RHS
+                self.A[gid_left][gid_left] += -2 * K_n_eff
+                self.A[gid_left][gid_right] += 2 * K_n_eff
 
-                A[gid_left][gid_left] += -2 * K_n_eff
-                A[gid_left][gid_right] += 2 * K_n_eff
+                self._node_treatment(I, nodes_weights, gid_right, gid_left,
+                                     v_ids, K_n_eff, D_JI)
+                self._node_treatment(K, nodes_weights, gid_right, gid_left,
+                                     v_ids, K_n_eff, D_JK)
+                self._node_treatment(J, nodes_weights, gid_right, gid_left,
+                                     v_ids, K_n_eff, D_JI, cross_2nd=D_JK,
+                                     is_J=-1)
 
-                b[0][gid_left] += -RHS
-
-        p = np.linalg.solve(A, b[0])
-        print("PRESSAO: ", p)
-        self.mb.tag_set_data(self.pressure_tag, volumes, p)
+        p = np.linalg.solve(self.A, self.b[0])
+        # print("PRESSAO: ", p)
+        self.mb.tag_set_data(self.pressure_tag, self.volumes, p)
         # self.mb.write_file("pressure_solution.vtk")
 
 
 class InterpolMethod:
+
+    def __init__(self, mesh_data):
+        self.mesh_data = mesh_data
+        self.mb = mesh_data.mb
+        self.mtu = mesh_data.mtu
 
     def by_least_squares(self):
         pass
@@ -224,8 +255,19 @@ class InterpolMethod:
     def by_volumes(self):
         pass
 
-    def by_inverse_distance(self):
-        pass
+    def by_inverse_distance(self, node):
+        coords_node = self.mb.get_coords([node])
+        vols_around = self.mtu.get_bridge_adjacencies(node, 0, 3)
+        weights = np.array([])
+        weight_sum = 0.0
+        for a_volume in vols_around:
+            vol_cent = self.mesh_data.get_centroid(a_volume)
+            inv_dist = 1/self.mesh_data.point_distance(coords_node, vol_cent)
+            weights = np.append(weights, inv_dist)
+            weight_sum += inv_dist
+        weights = weights / weight_sum
+        v_weights = {vol: weight for vol, weight in zip(vols_around, weights)}
+        return v_weights
 
     def by_lpew2(self):
         pass
