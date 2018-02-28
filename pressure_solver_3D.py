0ffa6b952592bc52ebc8c02435006eed6f171e03
Sidicley <sidicleyribeiro@gmail.com>
2018-02-28 09:27:19 -0300

    Repositioning weight methods

--- pressure_solver_3D.py
+++ pressure_solver_3D.py
@@ -269,6 +269,74 @@ class InterpolMethod:
         boundary_faces = (self.dirichlet_faces | self.neumann_faces)
         # print('ALL FACES', all_faces, len(all_faces))
         self.intern_faces = set(self.all_faces) - boundary_faces
+        
+    def by_inverse_distance(self, node):
+        coords_node = self.mb.get_coords([node])
+        vols_around = self.mtu.get_bridge_adjacencies(node, 0, 3)
+        weights = np.array([])
+        weight_sum = 0.0
+        for a_volume in vols_around:
+            vol_cent = self.mesh_data.get_centroid(a_volume)
+            inv_dist = 1/self.mesh_data.point_distance(coords_node, vol_cent)
+            weights = np.append(weights, inv_dist)
+            weight_sum += inv_dist
+        weights = weights / weight_sum
+        node_weights = {
+            vol: weight for vol, weight in zip(vols_around, weights)}
+        return node_weights
+
+    def by_least_squares(self, node):
+        crds_node = self.mb.get_coords([node])
+        vols_around = self.mtu.get_bridge_adjacencies(node, 0, 3)
+
+        R_x, R_y, R_z = 0.0, 0.0, 0.0
+        I_xx, I_yy, I_zz = 0.0, 0.0, 0.0
+        I_xy, I_xz, I_yz = 0.0, 0.0, 0.0
+
+        rel_vol_position = []
+        for a_vol in vols_around:
+            vol_cent = self.mesh_data.get_centroid(a_vol)
+            x_k, y_k, z_k = vol_cent - crds_node
+            rel_vol_position.append((x_k, y_k, z_k, a_vol))
+
+            R_x += x_k
+            R_y += y_k
+            R_z += z_k
+            I_xx += x_k * x_k
+            I_yy += y_k * y_k
+            I_zz += z_k * z_k
+            I_xy += x_k * y_k
+            I_xz += x_k * z_k
+            I_yz += y_k * z_k
+
+        G = I_xx * (I_yy*I_zz - I_yz*I_yz) + \
+            I_xy * (I_yz*I_xz - I_xy*I_zz) + \
+            I_xz * (I_xy*I_yz - I_yy*I_xz)
+
+        psi_x = (R_x * (I_yz*I_yz - I_yy*I_zz) +
+                 R_y * (I_xy*I_zz - I_yz*I_xz) +
+                 R_z * (I_yy*I_xz - I_xy*I_yz)) / G
+
+        psi_y = (R_x * (I_xy*I_zz - I_yz*I_xz) +
+                 R_y * (I_xz*I_xz - I_xx*I_zz) +
+                 R_z * (I_xx*I_yz - I_xy*I_xz)) / G
+
+        psi_z = (R_x * (I_yy*I_xz - I_xy*I_yz) +
+                 R_y * (I_xx*I_yz - I_xy*I_xz) +
+                 R_z * (I_xy*I_xy - I_xx*I_yy)) / G
+
+        num_vols = len(vols_around)
+        nodes_weights = {}
+
+        for x_k, y_k, z_k, volume in rel_vol_position:
+            numerator = 1.0 + x_k*psi_x + y_k*psi_y + z_k*psi_z
+            denominator = num_vols + R_x*psi_x + R_y*psi_y + R_z*psi_z
+            nodes_weights[volume] = numerator / denominator
+
+        return nodes_weights
+
+    def by_volumes(self):
+        pass
 
     def _get_volumes_sharing_face_and_node(self, node, volume):
         faces_in_volume = set(self.mtu.get_bridge_adjacencies(
@@ -324,74 +392,6 @@ class InterpolMethod:
                          E3(volume) * F4(volume) + G4(volume) * H4(volume) -
                          B2(volume) - F2(volume) - H2(volume))
         return volume_weight
-
-    def by_least_squares(self, node):
-        crds_node = self.mb.get_coords([node])
-        vols_around = self.mtu.get_bridge_adjacencies(node, 0, 3)
-
-        R_x, R_y, R_z = 0.0, 0.0, 0.0
-        I_xx, I_yy, I_zz = 0.0, 0.0, 0.0
-        I_xy, I_xz, I_yz = 0.0, 0.0, 0.0
-
-        rel_vol_position = []
-        for a_vol in vols_around:
-            vol_cent = self.mesh_data.get_centroid(a_vol)
-            x_k, y_k, z_k = vol_cent - crds_node
-            rel_vol_position.append((x_k, y_k, z_k, a_vol))
-
-            R_x += x_k
-            R_y += y_k
-            R_z += z_k
-            I_xx += x_k * x_k
-            I_yy += y_k * y_k
-            I_zz += z_k * z_k
-            I_xy += x_k * y_k
-            I_xz += x_k * z_k
-            I_yz += y_k * z_k
-
-        G = I_xx * (I_yy*I_zz - I_yz*I_yz) + \
-            I_xy * (I_yz*I_xz - I_xy*I_zz) + \
-            I_xz * (I_xy*I_yz - I_yy*I_xz)
-
-        psi_x = (R_x * (I_yz*I_yz - I_yy*I_zz) +
-                 R_y * (I_xy*I_zz - I_yz*I_xz) +
-                 R_z * (I_yy*I_xz - I_xy*I_yz)) / G
-
-        psi_y = (R_x * (I_xy*I_zz - I_yz*I_xz) +
-                 R_y * (I_xz*I_xz - I_xx*I_zz) +
-                 R_z * (I_xx*I_yz - I_xy*I_xz)) / G
-
-        psi_z = (R_x * (I_yy*I_xz - I_xy*I_yz) +
-                 R_y * (I_xx*I_yz - I_xy*I_xz) +
-                 R_z * (I_xy*I_xy - I_xx*I_yy)) / G
-
-        num_vols = len(vols_around)
-        nodes_weights = {}
-
-        for x_k, y_k, z_k, volume in rel_vol_position:
-            numerator = 1.0 + x_k*psi_x + y_k*psi_y + z_k*psi_z
-            denominator = num_vols + R_x*psi_x + R_y*psi_y + R_z*psi_z
-            nodes_weights[volume] = numerator / denominator
-
-        return nodes_weights
-
-    def by_volumes(self):
-        pass
-
-    def by_inverse_distance(self, node):
-        coords_node = self.mb.get_coords([node])
-        vols_around = self.mtu.get_bridge_adjacencies(node, 0, 3)
-        weights = np.array([])
-        weight_sum = 0.0
-        for a_volume in vols_around:
-            vol_cent = self.mesh_data.get_centroid(a_volume)
-            inv_dist = 1/self.mesh_data.point_distance(coords_node, vol_cent)
-            weights = np.append(weights, inv_dist)
-            weight_sum += inv_dist
-        weights = weights / weight_sum
-        node_weights = {
-            vol: weight for vol, weight in zip(vols_around, weights)}
-        return node_weights
 
     def by_lpew2(self, node):
         if node in self.dirichlet_nodes:
