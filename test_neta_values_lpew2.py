db73bd17c08d26981d08be8f467ab6807eecd503
Ricardo Lira <r.liraf@gmail.com>
2018-05-24 16:31:11 -0300

    neta func tests

--- test_neta_values_lpew2.py
+++ test_neta_values_lpew2.py
@@ -1,6 +1,7 @@
 import numpy as np
-
-node_coords = np.asarray([.0, .0, .0])
+np.seterr(all='ignore')
+nd = -1.
+node_coords = np.asarray([nd, nd, nd])
 nd_1_coords = np.asarray([-1., 0., 1.])
 nd_2_coords = np.asarray([1., 0., 1.])
 nd_3_coords = np.asarray([0., 1., 1.])
@@ -12,6 +13,10 @@ perm = [[1., 0., 0.],
         [0., 1., 0.],
         [0., 0., 1.]]
 
+adj_perm = [[1., 0., 0.],
+            [0., 1., 0.],
+            [0., 0., 1.]]
+
 big_tetra_vol = abs(np.linalg.det([nd_1_coords - node_coords,
                                    nd_2_coords - node_coords,
                                    nd_3_coords - node_coords]) / 6.)
@@ -22,11 +27,12 @@ T[2] = [np.average([node_coords[i], nd_1_coords[i],
                     nd_2_coords[i]]) for i in range(3)]
 T[3] = [np.average([node_coords[i], nd_2_coords[i]]) for i in range(3)]
 T[4] = [np.average([node_coords[i], nd_2_coords[i],
-                    nd_3_coords[i]])for i in range(3)]
+                    nd_3_coords[i]]) for i in range(3)]
 T[5] = [np.average([node_coords[i], nd_3_coords[i]]) for i in range(3)]
 T[6] = [np.average([node_coords[i], nd_3_coords[i],
-                    nd_1_coords[i]])for i in range(3)]
+                    nd_1_coords[i]]) for i in range(3)]
 T[7] = T[1]
+
 tetra_c_coords = [np.average([node_coords[i],
                               nd_1_coords[i],
                               nd_2_coords[i],
@@ -41,21 +47,21 @@ adj_centroid[2] = [np.average([node_coords[i],
                                nd_2_coords[i],
                                nd_4_coords[i]])for i in range(3)]
 adj_centroid[3] = [np.average([node_coords[i],
-                               nd_2_coords[i],
+                               nd_1_coords[i],
                                nd_3_coords[i],
                                nd_5_coords[i]])for i in range(3)]
 adj_centroid[4] = [np.average([node_coords[i],
-                               nd_2_coords[i],
+                               nd_1_coords[i],
                                nd_3_coords[i],
                                nd_5_coords[i]])for i in range(3)]
 adj_centroid[5] = [np.average([node_coords[i],
                                nd_3_coords[i],
-                               nd_1_coords[i],
+                               nd_2_coords[i],
                                nd_6_coords[i]])for i in range(3)]
 adj_centroid[6] = [np.average([node_coords[i],
                                nd_3_coords[i],
                                nd_6_coords[i],
-                               nd_1_coords[i]])for i in range(3)]
+                               nd_2_coords[i]])for i in range(3)]
 
 faces = {}
 faces[1] = [T[1], T[2]]
@@ -64,6 +70,12 @@ faces[3] = [T[3], T[4]]
 faces[4] = [T[4], T[5]]
 faces[5] = [T[5], T[6]]
 faces[6] = [T[6], T[1]]
+
+print(tetra_c_coords, T[2], adj_centroid[1])
+Q = node_coords
+vec_1 = np.cross(tetra_c_coords - Q, T[2] - Q)
+vec_2 = np.cross(T[2] - Q, adj_centroid[2] - Q)
+print(np.linalg.det([tetra_c_coords, Q, adj_centroid[1]]))
 
 
 def r_range(j, r):
@@ -80,24 +92,58 @@ def _get_volume(node, face_verts, tetra_centroid):
     tetra_vol = abs(np.linalg.det(tetra_vecs) / 6.)
     return tetra_vol
 
-neta_ratio = []
-for r in range(1, 7, 1):
-    volume = _get_volume(node_coords, faces[r], tetra_c_coords)
-    N_face = np.cross(T[r + 1], T[r]) / 2.
 
-    N_i = np.cross(tetra_c_coords, T[r + 1]) / 2
-    n_k = np.dot(N_i, np.dot(perm, N_face)) / (3. * volume)
+summ = 0.
+for j in range(1, 7, 1):
+    phi = 1.
+    for r in range(1, 7, 1):
+        volume = _get_volume(node_coords, faces[r], tetra_c_coords)
+        N_face = np.cross(T[r + 1], T[r]) / 2.
 
-    N_i = np.cross(T[r], tetra_c_coords) / 2
-    n_k_plus = np.dot(N_i, np.dot(perm, N_face)) / (3. * volume)
+        N_i = np.cross(tetra_c_coords, T[r + 1]) / 2
+        n_k = np.dot(N_i, np.dot(perm, N_face)) / (3. * volume)
+        n_k_hat = np.dot(N_face, np.dot(perm, N_face)) / (3. * volume)
 
-    volume = _get_volume(node_coords, faces[r], adj_centroid[r])
-    N_face_adj = -N_face
+        N_i = np.cross(T[r], tetra_c_coords) / 2
+        n_k_plus = np.dot(N_i, np.dot(perm, N_face)) / (3. * volume)
 
-    N_i_adj = np.cross(T[r + 1], adj_centroid[r]) / 2
-    n_k_adj = np.dot(N_i_adj, np.dot(perm, N_face_adj)) / (3. * volume)
+        volume = _get_volume(node_coords, faces[r], adj_centroid[r])
+        N_face_adj = N_face
 
-    N_i_adj = np.cross(adj_centroid[r], T[r]) / 2
-    n_k_adj_plus = np.dot(N_i_adj, np.dot(perm, N_face_adj)) / (3. * volume)
-    neta_ratio.append(np.nan_to_num((n_k_adj - n_k)/ (n_k_adj_plus - n_k_plus)))
-    print(n_k_adj, n_k, n_k_adj_plus, n_k_plus, neta_ratio)
+        N_i_adj = np.cross(T[r + 1], adj_centroid[r]) / 2
+        n_k_adj = np.dot(N_i_adj, np.dot(adj_perm, N_face_adj)) / (3. * volume)
+
+        N_i_adj = np.cross(adj_centroid[r], T[r]) / 2
+        n_k_adj_plus = np.dot(N_i_adj, np.dot(adj_perm, N_face_adj)) / (3. * volume)
+
+        neta_k_hat = n_k_hat / (n_k_adj_plus - n_k_plus)
+        neta = (n_k_adj - n_k) / (n_k_adj_plus - n_k_plus)
+        phi = phi * neta
+
+        prod = 1.
+        for r_star in r_range(j, r):
+            volume = _get_volume(node_coords, faces[r_star], tetra_c_coords)
+            N_face = np.cross(T[r_star + 1], T[r_star]) / 2.
+
+            N_i = np.cross(tetra_c_coords, T[r_star + 1]) / 2
+            n_k = np.dot(N_i, np.dot(perm, N_face)) / (3. * volume)
+
+            N_i = np.cross(T[r_star], tetra_c_coords) / 2
+            n_k_plus = np.dot(N_i, np.dot(perm, N_face)) / (3. * volume)
+
+            volume = _get_volume(node_coords, faces[r_star],
+                                 adj_centroid[r_star])
+            N_face_adj = N_face
+
+            N_i_adj = np.cross(T[r_star + 1], adj_centroid[r_star]) / 2
+            n_k_adj = np.dot(N_i_adj, np.dot(adj_perm,
+                                             N_face_adj)) / (3. * volume)
+
+            N_i_adj = np.cross(adj_centroid[r_star], T[r_star]) / 2
+            n_k_adj_plus = np.dot(N_i_adj, np.dot(adj_perm,
+                                                  N_face_adj)) / (3. * volume)
+            neta_ratio = (n_k_adj - n_k) / (n_k_adj_plus - n_k_plus)
+            prod = (-1) ** (1 + r_star) * neta_ratio * prod
+        summ += neta_k_hat * prod
+    print(summ)
+            #print(j, r, n_k_adj, n_k, n_k_adj_plus, n_k_plus, neta_ratio)
