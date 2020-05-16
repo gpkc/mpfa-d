26204dd3af9d2c35898385fff508f054b9af109e
ricardolira <r.liraf@gmail.com>
2020-05-16 15:20:00 -0300

    add README.md and refactored docker-compose manifest

--- solvers/FOUM.py
+++ solvers/FOUM.py
@@ -7,24 +7,28 @@ from preprocessor.mesh_preprocessor import MeshManager
 
 
 class TwoPhaseFlow:
-    def __init__(
-        self, filename, fluid_props, numericals
-    ):  # fluid_props = iOilSat, iWaterSat, visOil, visWater
-        self.mesh = MeshManager(filename, dim=3)
-        self.mesh.set_boundary_condition(
-            "WaterSat", {301: 0.2}, dim_target=3, set_nodes=True
-        )
+    def __init__(self, mesh_data, fluid_props):
+        self.mesh_data = mesh_data
+        self.mb = mesh_data.mb
+        self.mtu = mesh_data.mtu
+        self.fluid_props = fluid_props
+        # INIT DEPENDECIES AND VARS
+        # INIT SAT
+        # INIT VELOCITY FIELD
+        # INIT SPECIFIC MASS
+        # INIT VISCOSITY
 
-    def add_wells(
-        self, coords, operation, operatePressure=None, operateFlux=None
-    ):
-        pass
+    def fractional_flux(self, s_W):
+        f = s_W / (1 - s_W)
+        return f
 
-    def get_fluid_relative_perm(self, sw_field, volumes):
-        pass
-
-    def calc_brooks_korey_rel_perm(self, sw, a, b):
-        pass
+    def kr(self, s_W, krw0):
+        krw = krw0 * s_W ** 2
+        krow = (1 - s_W) ** 2
+        return krw, krow
 
     def calc_mobility(self, kr):
+        pass
+
+    def get_delta_t(self, cfl):
         pass
